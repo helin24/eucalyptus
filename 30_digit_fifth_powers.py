808314444802cfa89fa2ee76d466b97aac4f0766
helin24 <helin.shiah@gmail.com>
2015-11-03 20:02:50 -0500

    30 using number combinations

--- 30_digit_fifth_powers.py
+++ 30_digit_fifth_powers.py
@@ -1,14 +1,17 @@
 import time
+import itertools
 
 def sum_of_fifth_powers():
     # max number is less than 999999 because 9**5 * 6 is less than that number
     total = 0
-    for num in range(2, 300000):
-        digits = [int(x) for x in list(str(num))]
-        if sum(x**5 for x in digits) == num:
-            total += num
+    combinations = itertools.combinations_with_replacement(range(10), 6)
+    for c in combinations:
+        fifth_sum = sum(x**5 for x in c)
+        ordered_digits = sorted([int(x) for x in '0' * (6 - len(str(fifth_sum))) + str(fifth_sum)])
+        if list(c) == ordered_digits:
+            total += fifth_sum
 
-    return total
+    return total - 1
 
 start = time.time()
 print sum_of_fifth_powers()
