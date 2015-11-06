c559afc87ac2185ab4cab38931b78e3d872a382d
helin24 <helin.shiah@gmail.com>
2015-11-05 19:50:34 -0500

    slightly nicer 35

--- 35_circular_primes.py
+++ 35_circular_primes.py
@@ -2,41 +2,25 @@ import time
 
 def circular_primes(max_num):
     primes_hash = get_possible_primes(max_num)
-    circulars = [2, 5]
+    circulars = []
 
     for prime in primes_hash.keys():
         digits = []
-        remaining = prime
         circular = True
         
-        while remaining > 0:
+        size = len(str(prime))
+        remaining = prime
+        for _ in range(size - 1):
             remaining, digit = divmod(remaining, 10)
-            if digit % 2 == 0 or digit % 5 == 0:
+            remaining = remaining + digit * 10 ** (size - 1)
+            if remaining not in primes_hash:
                 circular = False
                 break
-            digits.append(digit)
-        if not circular:
-            continue
-
-        index = len(digits) - 2
-        while index >= 0:
-            multiplier = 1
-            num = 0 # 1 + 70 + 300  # 3 + 10 + 700
-            for d in digits[index + 1:]:
-                num += d * multiplier
-                multiplier *= 10
-            for d in digits[0:index + 1]:
-                num += d * multiplier
-                multiplier *= 10
-#            print "checking num %s" % (num)
-            if num not in primes_hash:
-                circular = False
-                break
-            index -= 1
 
         if circular:
             circulars.append(prime)
-        
+
+    print circulars
     return len(circulars)
 
 
